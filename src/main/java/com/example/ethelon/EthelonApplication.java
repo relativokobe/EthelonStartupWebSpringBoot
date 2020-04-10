526b5891fa441405c1e5f8eb6959132740657ce7
kobe <kobearelativo@gmail.com>
2020-04-10 23:10:51 +0800

    First commit. This commit serves as codebase for the project

---
+++ src/main/java/com/example/ethelon/EthelonApplication.java
@@ -0,0 +1,13 @@
+package com.example.ethelon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EthelonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EthelonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ethelon/EthelonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ethelon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EthelonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
